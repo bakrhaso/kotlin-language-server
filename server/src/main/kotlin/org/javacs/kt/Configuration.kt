129778d67eb67b380ee77844c03b0a073b86996a
bakrhaso <bardsore@gmail.com>
2024-07-13 12:33:42 +0200

    continuation indent should be 4 (same as regular indent)

--- server/src/main/kotlin/org/javacs/kt/Configuration.kt
+++ server/src/main/kotlin/org/javacs/kt/Configuration.kt
@@ -5,75 +5,83 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.JsonParseException
-import org.eclipse.lsp4j.InitializeParams
-import org.eclipse.lsp4j.DiagnosticSeverity
 import java.lang.reflect.Type
 import java.nio.file.InvalidPathException
 import java.nio.file.Path
 import java.nio.file.Paths
+import org.eclipse.lsp4j.DiagnosticSeverity
+import org.eclipse.lsp4j.InitializeParams
 
 public data class SnippetsConfiguration(
-    /** Whether code completion should return VSCode-style snippets. */
-    var enabled: Boolean = true
+        /** Whether code completion should return VSCode-style snippets. */
+        var enabled: Boolean = true
 )
 
 public data class CompletionConfiguration(
-    val snippets: SnippetsConfiguration = SnippetsConfiguration()
+        val snippets: SnippetsConfiguration = SnippetsConfiguration()
 )
 
 public data class DiagnosticsConfiguration(
-    /** Whether diagnostics are enabled. */
-    var enabled: Boolean = true,
-    /** The minimum severity of enabled diagnostics. */
-    var level: DiagnosticSeverity = DiagnosticSeverity.Hint,
-    /** The time interval between subsequent lints in ms. */
-    var debounceTime: Long = 250L
+        /** Whether diagnostics are enabled. */
+        var enabled: Boolean = true,
+        /** The minimum severity of enabled diagnostics. */
+        var level: DiagnosticSeverity = DiagnosticSeverity.Hint,
+        /** The time interval between subsequent lints in ms. */
+        var debounceTime: Long = 250L
 )
 
 public data class JVMConfiguration(
-    /** Which JVM target the Kotlin compiler uses. See Compiler.jvmTargetFrom for possible values. */
-    var target: String = "default"
+        /**
+         * Which JVM target the Kotlin compiler uses. See Compiler.jvmTargetFrom for possible
+         * values.
+         */
+        var target: String = "default"
 )
 
-public data class CompilerConfiguration(
-    val jvm: JVMConfiguration = JVMConfiguration()
-)
+public data class CompilerConfiguration(val jvm: JVMConfiguration = JVMConfiguration())
 
 public data class IndexingConfiguration(
-    /** Whether an index of global symbols should be built in the background. */
-    var enabled: Boolean = true
+        /** Whether an index of global symbols should be built in the background. */
+        var enabled: Boolean = true
 )
 
 public data class ExternalSourcesConfiguration(
-    /** Whether kls-URIs should be sent to the client to describe classes in JARs. */
-    var useKlsScheme: Boolean = false,
-    /** Whether external classes should be automatically converted to Kotlin. */
-    var autoConvertToKotlin: Boolean = false
+        /** Whether kls-URIs should be sent to the client to describe classes in JARs. */
+        var useKlsScheme: Boolean = false,
+        /** Whether external classes should be automatically converted to Kotlin. */
+        var autoConvertToKotlin: Boolean = false
 )
 
 data class InlayHintsConfiguration(
-    var typeHints: Boolean = false,
-    var parameterHints: Boolean = false,
-    var chainedHints: Boolean = false
+        var typeHints: Boolean = false,
+        var parameterHints: Boolean = false,
+        var chainedHints: Boolean = false
 )
 
 data class KtfmtConfiguration(
-    var style: String = "google",
-    var indent: Int = 4,
-    var maxWidth: Int = 100,
-    var continuationIndent: Int = 8,
-    var removeUnusedImports: Boolean = true,
+        var style: String = "google",
+        var indent: Int = 4,
+        var maxWidth: Int = 100,
+        var continuationIndent: Int = 4,
+        var removeUnusedImports: Boolean = true,
 )
 
 data class FormattingConfiguration(
-    var formatter: String = "ktfmt",
-    var ktfmt: KtfmtConfiguration = KtfmtConfiguration()
+        var formatter: String = "ktfmt",
+        var ktfmt: KtfmtConfiguration = KtfmtConfiguration()
 )
 
 fun getStoragePath(params: InitializeParams): Path? {
     params.initializationOptions?.let { initializationOptions ->
-        val gson = GsonBuilder().registerTypeHierarchyAdapter(Path::class.java, GsonPathConverter()).create()
-        val options = gson.fromJson(initializationOptions as JsonElement, InitializationOptions::class.java)
+        val gson =
+                GsonBuilder()
+                        .registerTypeHierarchyAdapter(Path::class.java, GsonPathConverter())
+                        .create()
+        val options =
+                gson.fromJson(
+                        initializationOptions as JsonElement,
+                        InitializationOptions::class.java
+                )
 
         return options?.storagePath
     }
@@ -82,14 +90,19 @@ fun getStoragePath(params: InitializeParams): Path? {
 }
 
 data class InitializationOptions(
-    // A path to a directory used by the language server to store data. Used for caching purposes.
-    val storagePath: Path?
+        // A path to a directory used by the language server to store data. Used for caching
+        // purposes.
+        val storagePath: Path?
 )
 
 class GsonPathConverter : JsonDeserializer<Path?> {
 
     @Throws(JsonParseException::class)
-    override fun deserialize(json: JsonElement, type: Type?, context: JsonDeserializationContext?): Path? {
+    override fun deserialize(
+            json: JsonElement,
+            type: Type?,
+            context: JsonDeserializationContext?
+    ): Path? {
         return try {
             Paths.get(json.asString)
         } catch (ex: InvalidPathException) {
@@ -100,12 +113,12 @@ class GsonPathConverter : JsonDeserializer<Path?> {
 }
 
 public data class Configuration(
-    val compiler: CompilerConfiguration = CompilerConfiguration(),
-    val completion: CompletionConfiguration = CompletionConfiguration(),
-    val diagnostics: DiagnosticsConfiguration = DiagnosticsConfiguration(),
-    val scripts: ScriptsConfiguration = ScriptsConfiguration(),
-    val indexing: IndexingConfiguration = IndexingConfiguration(),
-    val externalSources: ExternalSourcesConfiguration = ExternalSourcesConfiguration(),
-    val inlayHints: InlayHintsConfiguration = InlayHintsConfiguration(),
-    val formatting: FormattingConfiguration = FormattingConfiguration(),
+        val compiler: CompilerConfiguration = CompilerConfiguration(),
+        val completion: CompletionConfiguration = CompletionConfiguration(),
+        val diagnostics: DiagnosticsConfiguration = DiagnosticsConfiguration(),
+        val scripts: ScriptsConfiguration = ScriptsConfiguration(),
+        val indexing: IndexingConfiguration = IndexingConfiguration(),
+        val externalSources: ExternalSourcesConfiguration = ExternalSourcesConfiguration(),
+        val inlayHints: InlayHintsConfiguration = InlayHintsConfiguration(),
+        val formatting: FormattingConfiguration = FormattingConfiguration(),
 )
